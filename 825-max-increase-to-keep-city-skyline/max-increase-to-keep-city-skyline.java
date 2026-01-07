88627a0fa0cd66ae90223de223161c78f7d340d3
abhishekadake2003-prog <abhishekadake2003@gmail.com>
2026-01-07 23:07:35 +0530

    Time: 0 ms (100.00%) | Memory: 45.7 MB (25.00%) - LeetSync

---
+++ 825-max-increase-to-keep-city-skyline/max-increase-to-keep-city-skyline.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int maxIncreaseKeepingSkyline(int[][] grid) {
+        int n=grid.length,ans=0;
+        int rowmax[]=new int[n];
+        int colmax[]=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                rowmax[i]=Math.max(rowmax[i],grid[i][j]);
+                colmax[i]=Math.max(colmax[i],grid[j][i]);
+            }
+        }
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                ans+=Math.min(rowmax[i],colmax[j])-grid[i][j];
+            }
+        }
+        return ans;
+    }
+}
