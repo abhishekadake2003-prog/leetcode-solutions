59d4dcd1c49f7f5e37b3e693a6865af99b336bcf
abhishekadake2003-prog <abhishekadake2003@gmail.com>
2026-02-21 15:36:15 +0530

    Time: 8 ms (45.19%) | Memory: 42.7 MB (16.23%) - LeetSync

--- 767-prime-number-of-set-bits-in-binary-representation/prime-number-of-set-bits-in-binary-representation.java
+++ 767-prime-number-of-set-bits-in-binary-representation/prime-number-of-set-bits-in-binary-representation.java
@@ -4,18 +4,17 @@ class Solution {
         int ans=0;
         for(int i=left;i<=right;i++)
         {
-            int bit=calclulatebit(i);
-            if(set.contains(bit))ans++;
+            if(set.contains(Integer.bitCount(i)))ans++;
         }
         return ans;
     }
-    int calclulatebit(int n)
-    {
-        int bit=0;
-        while (n > 0) {
-            if((n & 1)==1)bit++;
-            n = n >> 1;
-        }
-        return bit;
-    }
+    // int calclulatebit(int n)
+    // {
+    //     int bit=0;
+    //     while (n > 0) {
+    //         if((n & 1)==1)bit++;
+    //         n = n >> 1;
+    //     }
+    //     return bit;
+    // }
 }
