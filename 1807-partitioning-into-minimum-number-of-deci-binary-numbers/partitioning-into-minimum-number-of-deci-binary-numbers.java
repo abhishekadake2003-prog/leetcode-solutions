eb9f3b3eae1086e82530696889c961a17dbe7bdf
abhishekadake2003-prog <abhishekadake2003@gmail.com>
2026-03-01 09:21:20 +0530

    Time: 7 ms (34.72%) | Memory: 47.3 MB (92.83%) - LeetSync

--- 1807-partitioning-into-minimum-number-of-deci-binary-numbers/partitioning-into-minimum-number-of-deci-binary-numbers.java
+++ 1807-partitioning-into-minimum-number-of-deci-binary-numbers/partitioning-into-minimum-number-of-deci-binary-numbers.java
@@ -3,7 +3,7 @@ class Solution {
         int max=0;
         for(int i=0;i<n.length();i++)
         {
-            max=Math.max(max,Integer.parseInt(""+n.charAt(i)));
+            max=Math.max(max,n.charAt(i)-'0');
         }
         return max;
     }
