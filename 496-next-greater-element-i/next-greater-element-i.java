521a986f97fbbfefeec03be512778dcd1d3a5f4d
abhishekadake2003-prog <abhishekadake2003@gmail.com>
2026-03-13 22:35:37 +0530

    Time: 5 ms (15.98%) | Memory: 44.5 MB (97.33%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.java
+++ 496-next-greater-element-i/next-greater-element-i.java
@@ -1,16 +1,17 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        int ans[]=new int[nums1.length];
         for(int i=0;i<nums1.length;i++)
         {
-            boolean pass=false,q=true;
+            boolean pass=false;
+            ans[i]=-1;
             for(int j=0;j<nums2.length;j++)
             {
                 if(pass)
                 {
                     if(nums2[j]>nums1[i])
                     {
-                        nums1[i]=nums2[j];
-                        q=false;
+                        ans[i]=nums2[j];
                         break;
                     }
                 }
@@ -19,9 +20,7 @@ class Solution {
                     pass=true;
                 }
             }
-            if(q)
-            nums1[i]=-1;
         }
-        return nums1;
+        return ans;
     }
 }
